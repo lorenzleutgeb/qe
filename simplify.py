f24f2caac047bde136f115963e2082ce1752c498
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2023-05-17 23:27:06 +0200

    simplify: Refactor to use match

--- simplify.py
+++ simplify.py
@@ -80,114 +80,86 @@ def simplify(
         else:
             return 0
 
-    # φ = ○ where ○ is ⊤ or ⊥
-    if isinstance(φ, TruthValue):
-        return φ
+    match φ:
+        case Implies(args=(φ, ψ)):  # φ → ψ
+            return rec(implies(φ, ψ))
+        case Equivalent(args=(φ, ψ)):  # φ ↔ ψ
+            return rec(And(implies(φ, ψ), implies(ψ, φ)))
+        case QuantifiedFormula(var=x, arg=ψ):  # φ = ○x.ψ where ○ is ∀ or ∃
+            ψ = rec(ψ)
+            return ψ if x not in ψ.get_vars().free else φ.func(x, ψ)
+        case TruthValue():  # φ = ○ where ○ is ⊤ or ⊥
+            return φ
+        case AtomicFormula():  # φ is an atom, e.g., s ○ t where ○ is one of >, ≥, <, ≤, =, ≠
+            return atom(φ)
+        case Not(arg=ψ):  # φ = ¬ψ
+            match rec(ψ):
+                case TruthValue():
+                    return encode(ψ is F)
+                case AndOr(args=args, dual_func=dual):
+                    # De Morgan's Law
+                    return rec(dual(*map(inv_not, args)))
+                case Implies(args=(φ, ψ)):
+                    # Definition of implication using ¬ and ∨ and De Morgan's Law
+                    return rec(And(φ, inv_not(ψ)))
+                case Equivalent(args=(φ, ψ)):
+                    # Definition of equivalence using ¬ and ∨ and De Morgan's Law
+                    return rec(Or(And(φ, inv_not(φ)), And(ψ, inv_not(ψ))))
+                case AtomicFormula(args=args, complement_func=complement):
+                    # Push negation down into atomic formula.
+                    return rec(complement(*args))
+                case _:
+                    raise NotImplementedError()
+        case AndOr(args=args, func=func):  # φ = ψ₁ ○ … ○ ψₙ where ○ is ∧ or ∨
+            (id, dual) = (T, F) if func == And else (F, T)
+            args = list(set(filter(lambda x: x is not id, map(rec, args))))
 
-    # φ = s ○ t where ○ is one of >, ≥, <, ≤, =, ≠
-    elif isinstance(φ, AtomicFormula):
-        return atom(φ)
+            if not args:
+                return id
+            if len(args) == 1:
+                return args[0]
+            elif dual in args:
+                return dual
 
-    # φ = φ' → φ''
-    elif isinstance(φ, Implies):
-        return rec(implies(*φ.args))
-
-    # φ = φ' ↔ φ''
-    elif isinstance(φ, Equivalent):
-        return rec(And(implies(*φ.args), implies(*reversed(φ.args))))
-
-    # φ = ○x.ψ where ○ is ∀ or ∃
-    elif isinstance(φ, QuantifiedFormula):
-        ψ = rec(φ.arg)
-        return ψ if φ.var not in ψ.get_vars().free else φ.func(φ.var, ψ)
-
-    # φ = ¬ψ
-    elif isinstance(φ, Not):
-        ψ = rec(φ.arg)
-        if isinstance(ψ, TruthValue):
-            return encode(ψ is F)
-        elif isinstance(ψ, AndOr):
-            # De Morgan's Law
-            return rec(ψ.dual_func(*[inv_not(x) for x in ψ.args]))
-        elif isinstance(ψ, Implies):
-            # Definition of implication using ¬ and ∨ and De Morgan's Law
-            return rec(And(ψ.args[0], inv_not(ψ.args[1])))
-        elif isinstance(ψ, Equivalent):
-            # Definition of euivalence using ¬ and ∨ and De Morgan's Law
-            return rec(
-                Or(
-                    And(ψ.args[0], inv_not(ψ.args[1])),
-                    And(ψ.args[1], inv_not(ψ.args[0])),
-                )
-            )
-        elif isinstance(ψ, AtomicFormula):
-            # Push negation down into atomic formula.
-            return rec(ψ.complement_func(*ψ.args))
-        else:
-            raise NotImplementedError(
-                "unreachable, cannot handle: " + str(ψ) + " " + str(type(ψ))
-            )
-
-    # φ = ψ₁ ○ … ○ ψₙ where ○ is ∧ or ∨
-    elif isinstance(φ, AndOr):
-        (id, dual) = (T, F) if isinstance(φ, And) else (F, T)
-        args = list(set(filter(lambda x: x is not id, map(rec, φ.args))))
-
-        if not args:
-            return id
-        if len(args) == 1:
-            return args[0]
-        elif dual in args:
-            return dual
-
-        # if None in args:
-        # raise ValueError("none in args 2")
-
-        # print(str(args))
-        if list_isinstance(args, AtomicFormula | None):
-            changed = True
-            while changed:
-                changed = False
-                for i in range(len(args)):
-                    ai = args[i]
-                    if ai is None:
-                        continue
-                    for j in range(i + 1, len(args)):
-                        aj = args[j]
-                        if aj is None:
+            if list_isinstance(args, AtomicFormula | None):
+                changed = True
+                while changed:
+                    changed = False
+                    for i in range(len(args)):
+                        ai = args[i]
+                        if ai is None:
                             continue
-                        merged = merge(φ.func, ai, aj)  # type: ignore
-                        if merged is None:
-                            continue
-                        elif isinstance(merged, TruthValue):
-                            if merged is dual:
-                                return dual
-                            else:
+                        for j in range(i + 1, len(args)):
+                            aj = args[j]
+                            if aj is None:
+                                continue
+                            merged = merge(φ.func, ai, aj)  # type: ignore
+                            if merged is None:
+                                continue
+                            elif isinstance(merged, TruthValue):
+                                if merged is dual:
+                                    return dual
+                                else:
+                                    args[i] = None
+                                    args[j] = None
+                                    changed = True
+                                    break
+                            elif merged is Merge.R:
                                 args[i] = None
-                                args[j] = None
+                                ai = None
                                 changed = True
                                 break
-                        elif merged is Merge.R:
-                            args[i] = None
-                            ai = None
-                            changed = True
-                            break
-                        elif merged is Merge.L:
-                            args[j] = None
-                            changed = True
-                        elif isinstance(merged, AtomicFormula):
-                            args[i] = merged
-                            ai = merged
-                            args[j] = None
-                        else:
-                            raise NotImplementedError()
-            args = list(filter(lambda x: x is not None, args))
+                            elif merged is Merge.L:
+                                args[j] = None
+                                changed = True
+                            elif isinstance(merged, AtomicFormula):
+                                args[i] = merged
+                                ai = merged
+                                args[j] = None
+                            else:
+                                raise NotImplementedError()
+                args = list(filter(lambda x: x is not None, args))
 
-        # if None in args:
-        # print("FOUND NONE 2")
-        # raise ValueError("none in args 2")
-        # print("sorting: " + str(args))
-        return φ.func(*sorted(args, key=cmp_to_key(formula_cmp)))  # type: ignore
-        # return φ.func(*args)  # type: ignore
+            return func(*sorted(args, key=cmp_to_key(formula_cmp)))  # type: ignore
 
     return φ
